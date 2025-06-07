0e427fd853c74f6c1665d1d3127e65933f9fbe13
Nishant Holla <nishantholla19@gmail.com>
2025-06-07 18:23:31 +0530

    nvim: Add options

---
+++ config/nvim/lua/config/options.lua
@@ -0,0 +1,21 @@
+local opt = vim.opt
+
+
+opt.colorcolumn = '100'
+opt.confirm = true
+opt.expandtab = true
+opt.foldcolumn = "1"
+opt.foldmethod = "manual"
+opt.ignorecase = true
+opt.number = true
+opt.relativenumber = true
+opt.shiftwidth = 2
+opt.signcolumn = 'yes:1'
+opt.smartcase = true
+opt.smartindent = true
+opt.softtabstop = 2
+opt.swapfile = false
+opt.tabstop = 2
+opt.termguicolors = true
+opt.undofile = true
+opt.wrap = false
