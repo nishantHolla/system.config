03cb1991ab2940d4af033e88a6106a3486a377fc
Nishant Holla <nishantholla19@gmail.com>
2025-06-07 18:40:35 +0530

    nvim: Add theme

---
+++ config/nvim/lua/config/theme.lua
@@ -0,0 +1,8 @@
+vim.cmd('colorscheme wildcharm')
+vim.cmd([[
+
+highlight StatusLine guifg=black ctermfg=black guibg=white ctermbg=white
+highlight FoldColumn guifg=#ffff00 guibg=#222222
+highlight SignColumn guibg=#222222
+
+]])
