3beaecbc91409bde5492367ac377509dbb73b683
Nishant Holla <nishantholla19@gmail.com>
2025-06-10 17:20:15 +0530

    awesome: Fix indent in theme

--- config/awesome/theme/init.lua
+++ config/awesome/theme/init.lua
@@ -18,33 +18,33 @@ theme_m.init_theme = function()
   b.font = b.default_font .. " 12"
   b.nerd_font = b.default_font
 
-	b.white = "#F0F1DF"
-	b.gray = "#2A2D30"
-	b.black = "#0D0D0D"
+  b.white = "#F0F1DF"
+  b.gray = "#2A2D30"
+  b.black = "#0D0D0D"
 
-	b.red = "#ff005c"
-	b.purple = "#ddaaff"
-	b.blue = "#60a3d9"
-	b.green = "#9aeba3"
-	b.yellow = "#ffe06a"
-	b.orange = "#F5793B"
+  b.red = "#ff005c"
+  b.purple = "#ddaaff"
+  b.blue = "#60a3d9"
+  b.green = "#9aeba3"
+  b.yellow = "#ffe06a"
+  b.orange = "#F5793B"
 
-	b.useless_gap = theme_m.dpi(3)
-	b.border_width = theme_m.dpi(3)
+  b.useless_gap = theme_m.dpi(3)
+  b.border_width = theme_m.dpi(3)
 
-	b.border_normal = b.black
-	b.border_focus = b.red
-	b.border_sticky = b.yellow
-	b.border_marked = b.blue
-	b.border_floating = b.purple
-	b.border_fullscreen = b.orange
+  b.border_normal = b.black
+  b.border_focus = b.red
+  b.border_sticky = b.yellow
+  b.border_marked = b.blue
+  b.border_floating = b.purple
+  b.border_fullscreen = b.orange
 
-	b.notification_font = b.font
-	b.notification_bg = b.black
-	b.notification_fg = b.white
-	b.notification_opacity = 0.7
-	b.notification_critical_bg = b.red
-	b.notification_critical_fg = b.black
+  b.notification_font = b.font
+  b.notification_bg = b.black
+  b.notification_fg = b.white
+  b.notification_opacity = 0.7
+  b.notification_critical_bg = b.red
+  b.notification_critical_fg = b.black
   b.notification_width = AwesomeWM.values.screen_width * 0.3
   b.notification_height = AwesomeWM.values.screen_height * 0.15
   b.notification_border_color = b.white
