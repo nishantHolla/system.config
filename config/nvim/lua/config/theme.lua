af86bb98b9aab2a5eb692663319e9b3156a0387c
Nishant Holla <nishantholla19@gmail.com>
2025-08-20 19:39:52 +0530

    nvim: Remove theme file

--- config/nvim/lua/config/theme.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd('colorscheme wildcharm')
-vim.cmd([[
-
-highlight StatusLine guifg=black ctermfg=black guibg=white ctermbg=white
-highlight FoldColumn guifg=#ffff00 guibg=#222222
-highlight SignColumn guibg=#222222
-
-]])
