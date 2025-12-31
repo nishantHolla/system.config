a60e00552ff85f100484d1957dc6ebbcd6d4f8a8
Nishant Holla <nishantholla19@gmail.com>
2025-12-31 16:26:53 +0530

    awesome: Add low_battery indicator

--- config/awesome/widgets/indicators/init.lua
+++ config/awesome/widgets/indicators/init.lua
@@ -3,11 +3,13 @@ local indicators_sm = {}
 indicators_sm.brightness = require("widgets.indicators.brightness")
 indicators_sm.volume = require("widgets.indicators.volume")
 indicators_sm.tags = require("widgets.indicators.tags")
+indicators_sm.low_battery = require("widgets.indicators.low_battery")
 
 indicators_sm.init = function(screen)
   indicators_sm.brightness.init(screen)
   indicators_sm.volume.init(screen)
   indicators_sm.tags.init(screen)
+  indicators_sm.low_battery.init(screen)
 end
 
 return indicators_sm
