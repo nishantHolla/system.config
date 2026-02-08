beb7af53d83d3861ba40cbaa44fdc7805e5d9969
Nishant Holla <nishantholla19@gmail.com>
2026-02-08 09:45:45 +0530

    nvim: Update theme

--- config/nvim/lua/config/theme.lua
+++ config/nvim/lua/config/theme.lua
@@ -1,3 +1,5 @@
-vim.cmd("colorscheme unokai")
+vim.cmd("colorscheme monokai-pro-classic")
 vim.api.nvim_set_hl(0, "WinBarNC", { bg = nil, fg = "#606060" })
 vim.api.nvim_set_hl(0, "WinBar", { bg = nil, fg = "#909090" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#0d0d0d"})
+
