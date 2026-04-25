d3bca4d67864a8b9304c26ffd331591558aeee21
Nishant Holla <nishantholla19@gmail.com>
2026-04-25 18:09:56 +0530

    nixos: Add package description

--- nixos/template/packages.nix
+++ nixos/template/packages.nix
@@ -5,12 +5,12 @@
   # System packages
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
-    distrobox
-    git
-    htop
-    podman-compose
-    vim
-    wget
-    xsecurelock
+    distrobox         # Wrapper around podman or docker to create and start containers
+    git               # Distributed version control system
+    htop              # Interactive process viewer
+    podman-compose    # Implementation of docker-compose with podman backend
+    vim               # Most popular clone of the VI editor
+    wget              # Tool for retrieving files using HTTP, HTTPS, and FTP
+    xsecurelock       # X11 screen lock utility with security in mind
   ];
 }
