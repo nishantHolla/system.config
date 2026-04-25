1b07ab6460f2d9bf3e0f31ea53f8406d85d43e7b
Nishant Holla <nishantholla19@gmail.com>
2026-04-25 18:10:57 +0530

    home: Move from unstable to stable branch

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration of nishant";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
