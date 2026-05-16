4311dc98d566cb91eb532264f3b8546f09493e3c
Nishant Holla <nishantholla19@gmail.com>
2026-05-16 14:09:40 +0530

    nvim: Update blink-cmp keymaps

--- config/nvim/lua/config/plugs/blink-cmp.lua
+++ config/nvim/lua/config/plugs/blink-cmp.lua
@@ -3,11 +3,11 @@ require("blink.cmp").setup({
         preset = "none",
 
         ['<a-b>'] = { 'scroll_documentation_up', 'fallback' },
-        ['<a-f>'] = { 'scroll_documentation_down', 'fallback'},
+        ['<a-f>'] = { 'scroll_documentation_down', 'fallback' },
         ['<a-d>'] = { 'show_documentation', 'fallback' },
         ['<a-Space>'] = { 'accept', 'show' },
-        ['<a-e>'] = { 'cancel', 'fallback'},
-        ['<a-l>'] = { 'accept', 'fallback'},
+        ['<a-e>'] = { 'cancel', 'fallback' },
+        ['<a-h>'] = { 'accept', 'fallback' },
         ['<a-k>'] = { 'select_prev', 'fallback' },
         ['<a-j>'] = { 'select_next', 'fallback' },
     },
@@ -18,6 +18,7 @@ require("blink.cmp").setup({
     completion = {
         documentation = { auto_show = false },
         menu = { draw = { components = { kind_icon = { text = function() return "" end, }, }, }, },
+        list = { selection = { preselect = false } }
     },
 
     sources = {
