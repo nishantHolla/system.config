a6713183d3cb59c641ec2b62e538521e0acde82b
Nishant Holla <nishantholla19@gmail.com>
2026-06-13 13:03:09 +0530

    cli: Update flake.nix

--- cli/flake.nix
+++ cli/flake.nix
@@ -14,6 +14,7 @@
                 devShells.default = pkgs.mkShell {
                     packages = with pkgs; [
                         python313
+                        python313Packages.typer
                         bitwarden-cli
                     ];
                 };
