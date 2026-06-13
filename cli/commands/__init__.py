fe41a8db361a7b46926d090f99efe1457ef62679
Nishant Holla <nishantholla19@gmail.com>
2026-06-13 13:05:06 +0530

    cli: Fix app linking

--- cli/commands/__init__.py
+++ cli/commands/__init__.py
@@ -1,1 +1,2 @@
 from . import nixos
+from . import home
--- cli/system.py
+++ cli/system.py
@@ -2,8 +2,12 @@
 
 import typer
 from config import values as v
+from commands import nixos
+from commands import home
 
 app = typer.Typer(help=v.SYSTEM_TYPER_HELP_STR, rich_markup_mode=None)
+app.add_typer(nixos.app, name="nixos")
+app.add_typer(home.app, name="home")
 
 if __name__ == "__main__":
     app()
