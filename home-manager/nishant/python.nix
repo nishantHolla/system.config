229a3a81433d1cc0862007dbdf1df7ad9913ab01
Nishant Holla <nishantholla19@gmail.com>
2026-06-14 17:05:57 +0530

    home: Add typer package for python

--- home-manager/nishant/python.nix
+++ home-manager/nishant/python.nix
@@ -8,6 +8,7 @@
         pyright                    # Type checker for the Python language
         python313                  # High-level dynamically-typed programming language
         python313Packages.debugpy  # Implementation of the Debug Adapter Protocol for Python
+        python313Packages.typer    # Library for building CLI applications
         ruff                       # Extremely fast Python linter and code formatter
     ];
 }
