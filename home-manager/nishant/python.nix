6110ae2e1e507773371975bbcb9ea1c06c000c99
Nishant Holla <nishantholla19@gmail.com>
2026-08-13 19:30:56 +0530

    home: Add uv package

--- home-manager/nishant/python.nix
+++ home-manager/nishant/python.nix
@@ -7,6 +7,7 @@
     home.packages = with pkgs; [
         pyright                    # Type checker for the Python language
         ruff                       # Extremely fast Python linter and code formatter
+        uv                         # Extremely fast Python package installer and resolver, written in Rust
 
         (python313.withPackages (ps: with ps; [
             debugpy  # Implementation of the Debug Adapter Protocol for Python
