d43ca8c36ac13ab458547383aa300713e5024aed
Nishant Holla <nishantholla19@gmail.com>
2026-08-21 12:15:00 +0530

    cli: Update entry point to add fedora command

--- cli/system.py
+++ cli/system.py
@@ -4,10 +4,12 @@ import typer
 from config import values as v
 from commands import nixos
 from commands import home
+from commands import fedora
 
 app = typer.Typer(help=v.SYSTEM_TYPER_HELP_STR, rich_markup_mode=None)
 app.add_typer(nixos.app, name="nixos")
 app.add_typer(home.app, name="home")
+app.add_typer(fedora.app, name="fedora")
 
 if __name__ == "__main__":
     app()
